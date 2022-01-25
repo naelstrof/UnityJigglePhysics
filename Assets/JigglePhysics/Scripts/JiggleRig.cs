ecff089c693e422aedd87d3febaccb435f47ee58
naelstrof <naelstrof@gmail.com>
2022-01-24 22:14:28 -0700

    root bones rotate now, non-animated bones are detected now

--- Assets/JigglePhysics/Scripts/JiggleRig.cs
+++ Assets/JigglePhysics/Scripts/JiggleRig.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class JiggleRig : MonoBehaviour {
 
-    //[System.Serializable] private List<Transform> ignoredTransforms;
+    [SerializeField]
+    private List<Transform> ignoredTransforms;
+
+    [Range(0f,1f)] [SerializeField]
+    float gravityMultiplier = 1f;
 
     private List<SimulatedPoint> simulatedPoints;
 
@@ -15,16 +19,20 @@ public class JiggleRig : MonoBehaviour {
     }
     private void LateUpdate() {
         foreach (SimulatedPoint simulatedPoint in simulatedPoints) {
+            if (simulatedPoint.cachedLocalBoneRotation == simulatedPoint.transform.localRotation) {
+                simulatedPoint.transform.localRotation = simulatedPoint.cachedInitialLocalBoneRotation;
+            }
             simulatedPoint.CacheAnimationPosition();
         }
         foreach (SimulatedPoint simulatedPoint in simulatedPoints) {
             simulatedPoint.DebugDraw(Color.green, true);
             if (simulatedPoint.child != null) {
                 Vector3 cachedAnimatedVector = simulatedPoint.child.cachedAnimatedPosition - simulatedPoint.cachedAnimatedPosition;
-                Vector3 simulatedVector = simulatedPoint.child.position - simulatedPoint.position;
+                Vector3 simulatedVector = simulatedPoint.child.interpolatedPosition - simulatedPoint.interpolatedPosition;
                 Quaternion animPoseToPhysicsPose = Quaternion.FromToRotation(cachedAnimatedVector, simulatedVector);
                 simulatedPoint.transform.rotation = animPoseToPhysicsPose * simulatedPoint.cachedBoneRotation;
             }
+            simulatedPoint.cachedLocalBoneRotation = simulatedPoint.transform.localRotation;
         }
     }
 
@@ -33,18 +41,22 @@ public class JiggleRig : MonoBehaviour {
             if (simulatedPoint.parent == null) {
                 simulatedPoint.SnapTo(transform);
             } else {
-                simulatedPoint.StepPhysics(Time.deltaTime);
-                simulatedPoint.ConstrainLength();
+                simulatedPoint.StepPhysics(Time.deltaTime, gravityMultiplier);
                 simulatedPoint.ConstrainAngle();
+                simulatedPoint.ConstrainLength();
             }
             simulatedPoint.DebugDraw(Color.black, false);
         }
+
     }
 
     private void CreateSimulatedPoints(Transform currentTransform, SimulatedPoint parentSimulatedPoint) {
         SimulatedPoint currentSimulatedPoint = new SimulatedPoint(currentTransform, parentSimulatedPoint, currentTransform.position);
         simulatedPoints.Add(currentSimulatedPoint);
         for (int i = 0; i < currentTransform.childCount; i++) {
+            if (ignoredTransforms.Contains(currentTransform.GetChild(i))) {
+                continue;
+            }
             CreateSimulatedPoints(currentTransform.GetChild(i), currentSimulatedPoint);
         }
     }

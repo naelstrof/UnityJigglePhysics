310de6c623942e4070b4bd97c367a705dd32a8fc
naelstrof <naelstrof@gmail.com>
2025-07-23 18:42:12 -0600

    Added simulated point

--- Assets/JiggleJob/JiggleBoneIntrinsic.cs
+++ Assets/JiggleJob/JiggleBoneIntrinsic.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public unsafe struct JiggleBoneIntrinsic {
+    public const int MAX_CHILDREN = 16;
     public int parentIndex;
     public fixed int childrenIndices[16];
 }
---
+++ Assets/JiggleJob/JiggleBoneSimulatedPoint.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class JiggleBoneSimulatedPoint {
+    public JiggleBoneParameters parameters;
+    public JiggleBoneIntrinsic intrinsic;
+    public int transformIndex;
+}

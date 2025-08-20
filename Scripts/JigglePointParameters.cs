fdb4f3518d9a656b10a39b62570f9f9c2db4d1be
naelstrof <naelstrof@gmail.com>
2025-08-19 20:52:49 -0600

    parameter work

--- Scripts/JigglePointParameters.cs
+++ Scripts/JigglePointParameters.cs
@@ -20,45 +20,67 @@ public struct JigglePointParameters {
 }
 
 [Serializable]
+public struct JiggleTreeCurvedFloat {
+    public float value;
+    public bool curveEnabled;
+    public AnimationCurve curve;
+    public float Evaluate(float normalizedDistanceFromRoot) {
+        return curveEnabled ? value * curve.Evaluate(normalizedDistanceFromRoot) : value;
+    }
+
+    public JiggleTreeCurvedFloat(float value) {
+        this.value = value;
+        curve = AnimationCurve.Constant(0f,1f,1f);
+        curveEnabled = false;
+    }
+}
+
+[Serializable]
 public struct JiggleTreeInputParameters {
     public bool advancedToggle;
     public bool collisionToggle;
     public bool angleLimitToggle;
-    public float stiffness;
-    public AnimationCurve stiffnessCurve;
+    public JiggleTreeCurvedFloat stiffness;
     public float soften;
-    public float angleLimit;
-    public AnimationCurve angleLimitCurve;
+    public JiggleTreeCurvedFloat angleLimit;
     public float angleLimitSoften;
     public float rootStretch;
-    public float stretch;
-    public AnimationCurve stretchCurve;
-    public float drag;
-    public AnimationCurve dragCurve;
-    public float airDrag;
-    public AnimationCurve airDragCurve;
-    public float gravity;
-    public AnimationCurve gravityCurve;
-    public float collisionRadius;
-    public AnimationCurve collisionRadiusCurve;
+    public JiggleTreeCurvedFloat stretch;
+    public JiggleTreeCurvedFloat drag;
+    public JiggleTreeCurvedFloat airDrag;
+    public JiggleTreeCurvedFloat gravity;
+    public JiggleTreeCurvedFloat collisionRadius;
     public float blend;
 
     public JigglePointParameters ToJigglePointParameters(float normalizedDistanceFromRoot) {
         return new JigglePointParameters {
             rootElasticity = advancedToggle ? 1f - rootStretch : 0f,
-            angleElasticity = Mathf.Pow(stiffness * stiffnessCurve.Evaluate(normalizedDistanceFromRoot), 2f),
+            angleElasticity = Mathf.Pow(stiffness.Evaluate(normalizedDistanceFromRoot), 2f),
             lengthElasticity = advancedToggle
-                ? Mathf.Pow(1f - stretch * stretchCurve.Evaluate(normalizedDistanceFromRoot), 2f)
+                ? Mathf.Pow(1f - stretch.Evaluate(normalizedDistanceFromRoot), 2f)
                 : 1f,
             elasticitySoften = advancedToggle ? Mathf.Pow(soften, 2f) : 0f,
-            gravityMultiplier = gravity * gravityCurve.Evaluate(normalizedDistanceFromRoot),
+            gravityMultiplier = gravity.Evaluate(normalizedDistanceFromRoot),
             angleLimited = angleLimitToggle,
-            angleLimit = angleLimit * angleLimitCurve.Evaluate(normalizedDistanceFromRoot),
+            angleLimit = angleLimit.Evaluate(normalizedDistanceFromRoot),
             angleLimitSoften = angleLimitSoften,
             blend = 1f,
-            drag = drag * dragCurve.Evaluate(normalizedDistanceFromRoot),
-            airDrag = airDrag * airDragCurve.Evaluate(normalizedDistanceFromRoot),
-            collisionRadius = collisionRadius * collisionRadiusCurve.Evaluate(normalizedDistanceFromRoot)
+            drag = drag.Evaluate(normalizedDistanceFromRoot),
+            airDrag = airDrag.Evaluate(normalizedDistanceFromRoot),
+            collisionRadius = collisionToggle ? collisionRadius.Evaluate(normalizedDistanceFromRoot) : 0f,
+        };
+    }
+
+    public static JiggleTreeInputParameters Default() {
+        return new JiggleTreeInputParameters() {
+            stiffness = new JiggleTreeCurvedFloat(0.8f),
+            angleLimit = new JiggleTreeCurvedFloat(0.5f),
+            stretch = new JiggleTreeCurvedFloat(0.1f),
+            rootStretch = 0.1f,
+            drag = new JiggleTreeCurvedFloat(0.1f),
+            airDrag = new JiggleTreeCurvedFloat(0f),
+            gravity = new JiggleTreeCurvedFloat(1f),
+            collisionRadius = new JiggleTreeCurvedFloat(0.1f),
         };
     }
 }

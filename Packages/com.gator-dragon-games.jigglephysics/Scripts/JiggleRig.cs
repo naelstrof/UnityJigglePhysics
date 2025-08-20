c3f7b431f3b9d13194643ab10abbdee1e3ba1b80
naelstrof <naelstrof@gmail.com>
2025-08-19 20:52:49 -0600

    parameter work

--- Packages/com.gator-dragon-games.jigglephysics/Scripts/JiggleRig.cs
+++ Packages/com.gator-dragon-games.jigglephysics/Scripts/JiggleRig.cs
@@ -21,7 +21,7 @@ public class JiggleRig : MonoBehaviour {
     [SerializeField] private Transform _rootBone;
     [FormerlySerializedAs("_advanced")] [SerializeField] protected bool advanced;
     [FormerlySerializedAs("_excludeRoot")] [SerializeField] protected bool excludeRoot;
-    [SerializeField] protected JiggleTreeInputParameters jiggleTreeInputParameters;
+    [SerializeField] protected JiggleTreeInputParameters jiggleTreeInputParameters = JiggleTreeInputParameters.Default();
     [FormerlySerializedAs("_excludedTransforms")] [SerializeField] protected List<Transform> excludedTransforms = new List<Transform>();
     [SerializeField] protected List<JiggleCollider> personalColliders = new List<JiggleCollider>();
     [SerializeField, HideInInspector] List<BoneNormalizedDistanceFromRoot> _boneNormalizedDistanceFromRootList;
@@ -32,6 +32,14 @@ public class JiggleRig : MonoBehaviour {
     public bool rootExcluded => excludeRoot;
     public Transform rootBone => _rootBone;
     public bool CheckExcluded(Transform t) => excludedTransforms.Contains(t);
+
+#if UNITY_EDITOR
+    [ContextMenu("Jiggle Physics/Reset to defaults")]
+    public void ResetToDefaults() {
+        jiggleTreeInputParameters = JiggleTreeInputParameters.Default();
+        EditorUtility.SetDirty(this);
+    }
+#endif
     
     public void GetJiggleColliders(List<JiggleCollider> colliders) {
         colliders.Clear();
@@ -79,13 +87,13 @@ public class JiggleRig : MonoBehaviour {
     void OnValidate() {
         isValid = _rootBone.IsChildOf(gameObject.transform);
         if (excludedTransforms == null) excludedTransforms = new List<Transform>();
-        ValidateCurve(ref jiggleTreeInputParameters.stiffnessCurve);
-        ValidateCurve(ref jiggleTreeInputParameters.angleLimitCurve);
-        ValidateCurve(ref jiggleTreeInputParameters.stretchCurve);
-        ValidateCurve(ref jiggleTreeInputParameters.dragCurve);
-        ValidateCurve(ref jiggleTreeInputParameters.airDragCurve);
-        ValidateCurve(ref jiggleTreeInputParameters.gravityCurve);
-        ValidateCurve(ref jiggleTreeInputParameters.collisionRadiusCurve);
+        ValidateCurve(ref jiggleTreeInputParameters.stiffness.curve);
+        ValidateCurve(ref jiggleTreeInputParameters.angleLimit.curve);
+        ValidateCurve(ref jiggleTreeInputParameters.stretch.curve);
+        ValidateCurve(ref jiggleTreeInputParameters.drag.curve);
+        ValidateCurve(ref jiggleTreeInputParameters.airDrag.curve);
+        ValidateCurve(ref jiggleTreeInputParameters.gravity.curve);
+        ValidateCurve(ref jiggleTreeInputParameters.collisionRadius.curve);
         BuildNormalizedDistanceFromRootList();
     }
 
@@ -129,13 +137,15 @@ public class JiggleRig : MonoBehaviour {
             serializedProperty,
             "StiffnessControl",
             nameof(JiggleTreeInputParameters.stiffness),
-            nameof(JiggleTreeInputParameters.stiffnessCurve),
-            "Stiffness"
+            "Stiffness",
+            0.3f,
+            1f,
+            "Stiffness controls how strongly the bone returns to its rest pose. A value of 1 makes it immovable, while a value of 0 makes it fall freely."
         );
 
         var angleLimitToggleElement = visualElement.Q<Toggle>("AngleLimitToggle");
-        angleLimitToggleElement.BindProperty(
-            serializedProperty.FindPropertyRelative(nameof(JiggleTreeInputParameters.angleLimitToggle)));
+        angleLimitToggleElement.BindProperty(serializedProperty.FindPropertyRelative(nameof(JiggleTreeInputParameters.angleLimitToggle)));
+        angleLimitToggleElement.tooltip = "Enable or disable angle limit.";
         angleLimitToggleElement.Q<Label>().text = "Angle Limit";
 
         var angleLimitSection = visualElement.Q<VisualElement>("AngleLimitSection");
@@ -148,53 +158,64 @@ public class JiggleRig : MonoBehaviour {
             serializedProperty,
             "AngleLimitControl",
             nameof(JiggleTreeInputParameters.angleLimit),
-            nameof(JiggleTreeInputParameters.angleLimitCurve),
-            "Angle Limit"
+            "Angle Limit",
+            0f,
+            1f,
+            "Angle Limit controls the maximum angle deviation from the bone's rest position. 0 means no limit, while 1 represents a 90 degree limit."
         );
         SetSlider(
             visualElement,
             serializedProperty,
             "AngleLimitSoftenSlider",
             nameof(JiggleTreeInputParameters.angleLimitSoften),
-            "Angle Limit Soften"
+            "Angle Limit Soften",
+            "Softens the angle limit to prevent hard stops, 0 is hard, 1 is soft."
         );
         SetSlider(
             visualElement,
             serializedProperty,
             "SoftenSlider",
             nameof(JiggleTreeInputParameters.soften),
-            "Soften"
+            "Soften",
+            "Weakens the stiffness of the bone when it's closer to the target pose. Prevents large deformations, while still looking very soft."
         );
         SetSlider(
             visualElement,
             serializedProperty,
             "RootStretchSlider",
             nameof(JiggleTreeInputParameters.rootStretch),
-            "Root Stretch"
+            "Root Stretch",
+            "Allows the root bone to translate. 0 means the root bone is fixed in place, while 1 means it can stretch freely."
         );
         SetCurvableSlider(
             visualElement,
             serializedProperty,
             "StretchControl",
             nameof(JiggleTreeInputParameters.stretch),
-            nameof(JiggleTreeInputParameters.stretchCurve),
-            "Stretch"
+            "Stretch",
+            0f,
+            1f,
+            "Stretch controls the elasticity of the bone length, where 0 is no stretch and 1 is full stretch."
         );
         SetCurvableSlider(
             visualElement,
             serializedProperty,
             "DragControl",
             nameof(JiggleTreeInputParameters.drag),
-            nameof(JiggleTreeInputParameters.dragCurve),
-            "Drag"
+            "Drag",
+            0f,
+            1f,
+            "Drag controls the tendency for the bone to stop oscillating, where 0 is maximum oscillations and 1 is zero oscillations."
         );
         SetCurvableSlider(
             visualElement,
             serializedProperty,
             "AirDragControl",
             nameof(JiggleTreeInputParameters.airDrag),
-            nameof(JiggleTreeInputParameters.airDragCurve),
-            "Air Drag"
+            "Air Drag",
+            0f,
+            1f,
+            "Air Drag controls how much resistance the bone experiences in air, 0 is no resistance and 1 is maximum resistance."
         );
 
         SetCurvableFloat(
@@ -202,7 +223,6 @@ public class JiggleRig : MonoBehaviour {
             serializedProperty,
             "GravityControl",
             nameof(JiggleTreeInputParameters.gravity),
-            nameof(JiggleTreeInputParameters.gravityCurve),
             "Gravity"
         );
 
@@ -211,7 +231,6 @@ public class JiggleRig : MonoBehaviour {
             serializedProperty,
             "CollisionRadiusControl",
             nameof(JiggleTreeInputParameters.collisionRadius),
-            nameof(JiggleTreeInputParameters.collisionRadiusCurve),
             "Collision Radius"
         );
 
@@ -221,8 +240,8 @@ public class JiggleRig : MonoBehaviour {
         advancedToggleElement.Q<Label>().text = "Advanced";
 
         var collisionToggleElement = visualElement.Q<Toggle>("CollisionToggle");
-        collisionToggleElement.BindProperty(
-            serializedProperty.FindPropertyRelative(nameof(JiggleTreeInputParameters.collisionToggle)));
+        collisionToggleElement.BindProperty(serializedProperty.FindPropertyRelative(nameof(JiggleTreeInputParameters.collisionToggle)));
+        collisionToggleElement.tooltip = "Enable or disable collision with Jiggle Colliders";
         collisionToggleElement.Q<Label>().text = "Collision";
 
         var advancedSection = visualElement.Q<VisualElement>("AdvancedSection");
@@ -240,31 +259,61 @@ public class JiggleRig : MonoBehaviour {
         return visualElement;
     }
 
-    void SetSlider(VisualElement visualElement, SerializedProperty serializedProperty, string id, string parameter,
-        string name) {
+    void SetSlider(VisualElement visualElement, SerializedProperty serializedProperty, string id, string parameter, string propertyName, string tooltip) {
         var sliderElement = visualElement.Q<Slider>(id);
-        sliderElement.BindProperty(serializedProperty.FindPropertyRelative(parameter));
-        sliderElement.Q<Label>().text = name;
+        var property = serializedProperty.FindPropertyRelative(parameter);
+        sliderElement.BindProperty(property);
+        sliderElement.tooltip = tooltip;
+        sliderElement.Q<Label>().text = propertyName;
     }
 
-    void SetCurvableSlider(VisualElement visualElement, SerializedProperty serializedProperty, string id,
-        string sliderParameter, string curveParameter, string name) {
+    void SetCurvableSlider(VisualElement visualElement, SerializedProperty serializedProperty, string id, string curvableFloatParameter, string propertyName, float min, float max, string tooltip) {
+        var curvableFloatProperty = serializedProperty.FindPropertyRelative(curvableFloatParameter);
+        var floatProperty = curvableFloatProperty.FindPropertyRelative(nameof(JiggleTreeCurvedFloat.value));
+        var toggleProperty = curvableFloatProperty.FindPropertyRelative(nameof(JiggleTreeCurvedFloat.curveEnabled));
+        var curveProperty = curvableFloatProperty.FindPropertyRelative(nameof(JiggleTreeCurvedFloat.curve));
+        
         var sliderElement = visualElement.Q<VisualElement>(id);
         var sliderElementSlider = sliderElement.Q<Slider>("CurvableSlider");
-        sliderElementSlider.BindProperty(serializedProperty.FindPropertyRelative(sliderParameter));
-        sliderElementSlider.Q<Label>().text = name;
+        sliderElementSlider.BindProperty(floatProperty);
+        sliderElementSlider.lowValue = min;
+        sliderElementSlider.highValue = max;
+        sliderElementSlider.tooltip = tooltip;
+        sliderElementSlider.Q<Label>().text = propertyName;
+        
         var stiffnessCurveElement = sliderElement.Q<CurveField>("CurvableCurve");
-        stiffnessCurveElement.BindProperty(serializedProperty.FindPropertyRelative(curveParameter));
+        stiffnessCurveElement.tooltip = tooltip;
+        stiffnessCurveElement.BindProperty(curveProperty);
+        
+        var toggle = sliderElement.Q<Toggle>("CurvableToggle");
+        toggle.BindProperty(toggleProperty);
+        toggle.tooltip = "Enable or disable curve sampling for this value based on the normalized distance from the root.";
+        stiffnessCurveElement.style.display = toggleProperty.boolValue ? DisplayStyle.Flex : DisplayStyle.None;
+        toggle.RegisterValueChangedCallback(evt => {
+            stiffnessCurveElement.style.display = evt.newValue ? DisplayStyle.Flex : DisplayStyle.None;
+        });
     }
 
-    void SetCurvableFloat(VisualElement visualElement, SerializedProperty serializedProperty, string id,
-        string floatParameter, string curveParameter, string name) {
+    void SetCurvableFloat(VisualElement visualElement, SerializedProperty serializedProperty, string id, string curvableFloatParameter, string propertyName) {
+        var curvableFloatProperty = serializedProperty.FindPropertyRelative(curvableFloatParameter);
+        var floatProperty = curvableFloatProperty.FindPropertyRelative(nameof(JiggleTreeCurvedFloat.value));
+        var toggleProperty = curvableFloatProperty.FindPropertyRelative(nameof(JiggleTreeCurvedFloat.curveEnabled));
+        var curveProperty = curvableFloatProperty.FindPropertyRelative(nameof(JiggleTreeCurvedFloat.curve));
+        
         var sliderElement = visualElement.Q<VisualElement>(id);
         var curvableFloat = sliderElement.Q<FloatField>("CurvableFloat");
-        curvableFloat.BindProperty(serializedProperty.FindPropertyRelative(floatParameter));
-        curvableFloat.Q<Label>().text = name;
+        curvableFloat.BindProperty(floatProperty);
+        curvableFloat.Q<Label>().text = propertyName;
+        
         var stiffnessCurveElement = sliderElement.Q<CurveField>("CurvableCurve");
-        stiffnessCurveElement.BindProperty(serializedProperty.FindPropertyRelative(curveParameter));
+        stiffnessCurveElement.BindProperty(curveProperty);
+        
+        var toggle = sliderElement.Q<Toggle>("CurvableToggle");
+        toggle.BindProperty(toggleProperty);
+        stiffnessCurveElement.style.display = toggleProperty.boolValue ? DisplayStyle.Flex : DisplayStyle.None;
+        toggle.RegisterValueChangedCallback(evt => {
+            stiffnessCurveElement.style.display = evt.newValue ? DisplayStyle.Flex : DisplayStyle.None;
+        });
     }
 
 #endif

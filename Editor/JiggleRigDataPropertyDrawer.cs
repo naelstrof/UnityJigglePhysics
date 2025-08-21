8aa592762b48b1a767dbdcd9f02048bc67d2867f
naelstrof <naelstrof@gmail.com>
2025-08-20 20:26:31 -0600

    major serialization and ui rework

---
+++ Editor/JiggleRigDataPropertyDrawer.cs
@@ -0,0 +1,52 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace GatorDragonGames.JigglePhysics {
+
+[CustomPropertyDrawer(typeof(JiggleRigData))]
+public class JiggleRigDataPropertyDrawer : PropertyDrawer {
+    public override VisualElement CreatePropertyGUI(SerializedProperty property) {
+        var visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath("3b91b5cf6b975bd4d83d8a940258c420"));
+        var visualElement = new VisualElement();
+        visualTreeAsset.CloneTree(visualElement);
+        
+        var foldout = visualElement.Q<Foldout>("Foldout");
+        foldout.BindProperty(property.FindPropertyRelative("foldout"));
+        
+        var rootElement = visualElement.Q<ObjectField>("RootField");
+        rootElement.objectType = typeof(Transform);
+        rootElement.BindProperty(property.FindPropertyRelative("rootBone"));
+        rootElement.Q<Label>().text = "Root Transform";
+
+        var excludeRootToggleElement = visualElement.Q<Toggle>("ExcludeRootToggle");
+        excludeRootToggleElement.BindProperty(property.FindPropertyRelative("excludeRoot"));
+        excludeRootToggleElement.Q<Label>().text = "Motionless Root";
+
+        var excludedTransformsElement = visualElement.Q<PropertyField>("IgnoredTransformsField");
+        excludedTransformsElement.BindProperty(property.FindPropertyRelative("excludedTransforms"));
+
+        var personalCollidersElement = visualElement.Q<PropertyField>("PersonalCollidersField");
+        personalCollidersElement.BindProperty(property.FindPropertyRelative("jiggleColliders"));
+        
+        var container = visualElement.Q<VisualElement>("Contents");
+        var rig = (JiggleRigData)property.boxedValue;
+        container.Add(rig.GetInspectorVisualElement(property.FindPropertyRelative("jiggleTreeInputParameters")));
+        
+        var rootSection = visualElement.Q<VisualElement>("RootSection");
+        excludeRootToggleElement.RegisterValueChangedCallback(evt => {
+            rootSection.style.display = evt.newValue ? DisplayStyle.None : DisplayStyle.Flex;
+        });
+
+        return visualElement;
+    }
+    
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.LabelField(position, "Jiggle Physics doesn't support IMGUI inspectors, sorry!");
+    }
+}
+
+}
+#endif

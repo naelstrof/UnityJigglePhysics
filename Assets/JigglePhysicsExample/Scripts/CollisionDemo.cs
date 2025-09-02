87c1dc80adaf059a0bcd7840bbac64b097c72547
naelstrof <naelstrof@gmail.com>
2025-09-02 16:04:26 -0600

    some demo cleanup

---
+++ Assets/JigglePhysicsExample/Scripts/CollisionDemo.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDemo : MonoBehaviour {
+    [SerializeField]
+    private GameObject colliderPrefab;
+    [SerializeField]
+    private GameObject testObject;
+
+    private List<GameObject> colliders;
+
+    void Start() {
+        colliders = new List<GameObject>();
+        StartCoroutine(SpawnColliders());
+        StartCoroutine(MoveColliders());
+        StartCoroutine(TestCollider());
+    }
+
+    void Update() {
+        transform.position += (Vector3.forward * Mathf.PerlinNoise1D(Time.time) + Vector3.right * Mathf.PerlinNoise1D(Time.time*3f)) * (Time.deltaTime);
+    }
+    
+    IEnumerator SpawnColliders() {
+        while (isActiveAndEnabled) {
+            var obj = Instantiate(colliderPrefab, transform, true);
+            obj.transform.position = new Vector3(Mathf.PerlinNoise1D(Time.time*13f)-0.5f, 0f, Mathf.PerlinNoise1D(Time.time*17f)-0.5f)*100f;
+            obj.transform.localScale = Vector3.one * UnityEngine.Random.Range(0.05f, 0.4f);
+            colliders.Add(obj);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    IEnumerator MoveColliders() {
+        while (isActiveAndEnabled) {
+            foreach (var collider in colliders) {
+                collider.transform.position += Vector3.up*(Mathf.Sin(Time.time*3f) * Time.deltaTime);
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TestCollider() {
+        while (isActiveAndEnabled) {
+            if (colliders.Count == 0) {
+                yield return null;
+            }
+            var selectedCollider = colliders[UnityEngine.Random.Range(0, colliders.Count)];
+            testObject.transform.position = selectedCollider.transform.position +Vector3.forward*0.25f;
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
